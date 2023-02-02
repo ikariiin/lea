cc59ba023e8da9523bb80e70dd14157021819888
Gourab Nag <gourabnag12@gmail.com>
2023-02-02 09:26:47 +0530

    change env to prod

--- dist/914.bundle.js
+++ dist/914.bundle.js
@@ -1,2 +1,2 @@
-"use strict";(self.webpackChunkfrontend=self.webpackChunkfrontend||[]).push([[914],{3914:(e,r,n)=>{n.r(r),n.d(r,{useQuery:()=>s});var u=n(2784),f=n(3557),t=$RefreshSig$();function s(){t();var e=(0,f.TH)();return u.useMemo((function(){return new URLSearchParams(e.search)}),[e])}t(s,"2C3fFgfpLfpeuWv7tC/2m+4rlHY=",!1,(function(){return[f.TH]}))}}]);
+"use strict";(self.webpackChunkfrontend=self.webpackChunkfrontend||[]).push([[914],{3914:(e,r,n)=>{n.r(r),n.d(r,{useQuery:()=>a});var u=n(2784),s=n(3557);function a(){var e=(0,s.TH)();return u.useMemo((function(){return new URLSearchParams(e.search)}),[e])}}}]);
 //# sourceMappingURL=914.bundle.js.map
